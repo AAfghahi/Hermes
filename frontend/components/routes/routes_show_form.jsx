265915ebd614de8405b8fa790b038ff4c2620590
AAfghahi <arash.afghahi@gmail.com>
2020-09-24 22:16:54 -0400

    created directions renderer

--- frontend/components/routes/routes_show_form.jsx
+++ frontend/components/routes/routes_show_form.jsx
@@ -15,38 +15,48 @@ const getCoordsObj = latLng =>({
 class RouteShow extends React.Component{
     constructor(props){
         super(props);
-        this.snappedCoordinates = [];
+        this.points = [];
+        
+
+        
+       
+        this.createRoute.bind(this);
+        this.registerListeners.bind(this);
+        this.addMarker.bind(this);
+        this.addLatLang.bind(this);
     }
 
     componentDidMount(){
-       
+        this.map = new google.maps.Map(
+            this.mapdiv,
+            {zoom:14, center: myLatlng}
+        );
+        
         this.poly = new google.maps.Polyline({
             strokeColor: '#FFFFFF',
             strokeOpacity: 1.0,
             strokeWeight: 3,
             zIndex:1,
-            path:this.snappedCoordinates
+            
         });
-
         this.border = new google.maps.Polyline({
             strokeColor:'#FF4500',
             strokeOpacity: 0.5,
             strokeWeight: 7,
-            path:this.snappedCoordinates
+           
         });
 
-
-        this.map = new google.maps.Map(
-            this.mapdiv,
-            {zoom:14, center: myLatlng}
-        );
-
+        this.directionsService = new google.maps.DirectionsService();
+        this.directionsRenderer = new google.maps.DirectionsRenderer({
+            draggable: true,
+            map: this.map,
+            preserveViewport:true,
+        });
         const map = this.map;
-        
+        this.directionsRenderer.setMap(map);
         this.registerListeners();
-        this.setState({});
-        this.border.setMap(map);
-        this.poly.setMap(map);
+        
+  
     
     }
     handleClick(coords){
@@ -57,24 +67,24 @@ class RouteShow extends React.Component{
         
        
         google.maps.event.addListener(this.map, 'click', (event)=>{
-            this.addMarker(event.latLng, this.map);
-            this.addLatLang(event);
+            this.addLatLang(event.latLng);
+            this.addMarker();
+            this.createRoute();
         });
 
 
 
       }
 
-    addMarker(location= google.maps.LatLngLiteral, map=google.maps.Map){
-        const point = {
-            url:window.circleURL,
-            scaledSize: new google.maps.Size(10,10)
-            
+    addMarker(){
+        const map  = this.map;
+        const marker = this.points[this.points.length -1];
+        const position = {
+            lat: marker.location.lat(),
+            lng: marker.location.lng()
         };
-
         new google.maps.Marker({
-            position: location,
-            label: '',
+            position: position,
             map: map,
             icon:{
                 path:google.maps.SymbolPath.CIRCLE,
@@ -83,19 +93,33 @@ class RouteShow extends React.Component{
                 fillColor:'white',
                 strokeColor:'#D3D3D3'
             },
-            draggable:true
         });
+        this.createRoute();
         }
 
-        addLatLang(event){
+        addLatLang(location){
 
-            const borderPath = this.border.getPath();
-            const path = this.poly.getPath();
-            borderPath.push(event.latLng);
-            path.push(event.latLng);
+            this.points.push({
+                location:location
+            });
             
         }
+        createRoute(){
+            const _self = this;
+            const waypnt = this.points.slice(1);
+            const request ={
+                origin: this.points[0].location,
+                destination: this.points[this.points.length -1],
+                waypoints: waypnt.map(point => ({location: point.location})),
+                travelMode: 'BICYCLING'
+            };
 
+            this.directionsService.route(request, function(result,status){
+                if(status === 'OK'){
+                    _self.directionsRenderer.setDirections(result);
+                }
+            });
+        }
      
 
     render(){
